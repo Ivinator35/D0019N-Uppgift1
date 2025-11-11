aafa7d6af9185c9870d002e7289a35bdc935a3d2
isakNn <isak.niemi@live.se>
2025-11-11 16:43:46 +0100

    fixat min del

--- Uppgift1/src/Player.java
+++ Uppgift1/src/Player.java
@@ -1,2 +1,11 @@
 public class Player {
+    private String playerName;
+
+    public void SetName (name) {
+        playerName = name
+    }
+    
+    public String GetName() {
+        return playerName;
+    }
 }
